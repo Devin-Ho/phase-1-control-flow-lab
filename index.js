bec95627c2f9cbe02b0f383f2017b7d9891723af
Devin Ho <djxho36@gmail.com>
2022-04-06 10:07:50 -0400

    commit tree c5922777db1635288f88f63f921ea0b2c2542aa1

--- index.js
+++ index.js
@@ -1,11 +1,36 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(distance){
   // Write your code here!
+  let response;
+  if (distance <= 400) {
+    return response = "This one is on me!";
+  } else if (400 < distance && distance < 2000) {
+    return response = "That will be twenty bucks.";
+  } else if ( distance > 2000 && distance < 2500){
+      return response = "I will gladly take your thirty bucks.";
+    } else if (distance >= 2500){
+      return response = "No can do.";
+      }
+    }
+
+
+function ternaryCheckCity(city){
+  // Write your code here!
+  const message = city === "NYC" ? "Ok, sounds good." : "No go.";
+  return message; 
 }
 
-function ternaryCheckCity(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
-}
-
-function switchOnCharmFromTip(){
-  // Write your code here!
+  let message; 
+  switch (tip) {
+        case 'generous':
+          return message = "Thank you so much.";
+          break;
+        case 'not as generous':
+          return message = "Thank you."
+          break; 
+        case 'thanks for everything':
+          return message = "Bye.";
+          break;
+  }
 }
